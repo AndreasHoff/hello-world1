80ac2f284ee074fa5b3423cf44ff46f92f2637cb
Andreas <ah@asseco.dk>
2022-03-23 09:43:39 +0100

    hehe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,9 +18,31 @@ export class AppComponent {
     isLiked: true
   }
 
-  courses = [
-   
-  ]
+  onAdd() {
+    this.courses.push({ id: 4, name: 'course4' });
+  }
+
+  onRemove(course) {
+    let index = this.courses.indexOf(course)
+    this.courses.splice(index, 1);
+  }
+
+  onChange(course){
+    course.name = "Updated";  
+  }
+
+  courses;
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1 '},
+      { id: 2, name: 'course2 '},
+      { id: 3, name: 'course3 '},
+    ]
+  }
+
+  trackCourse(index, course) {
+    course ? course.id : undefined;
+  }
 
   onStarChanged(eventArgs: StarChangedEventArgs){
     console.log('star changed', eventArgs)
